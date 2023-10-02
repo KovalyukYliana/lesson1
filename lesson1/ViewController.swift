bf1ea91f8f36b888dbf3a16db1ff7bfcb5f43ad0
KovalyukYliana <m2202457@edu.misis.ru>
2023-10-02 17:04:27 +0300

    Initial Commit

---
+++ lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by Yliana Kovalyuk on 02.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
